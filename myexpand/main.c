8f06e4ef02f7a2424b1777376eb548c88e1988d9
Jakob <e11809618@student.tuwien.ac.at>
2019-10-18 12:46:05 +0200

    added myexpand

---
+++ myexpand/main.c
@@ -0,0 +1,96 @@
+/*
+	@author: Jakob G. Maier 11809618
+	@date: 18.10.19
+
+
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>  
+
+void usage(char *myprog)
+{
+	fprintf(stderr, "Usage: %s [-t tabstop] [-o outfile] [file...]\n", myprog);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+	int c;
+	char *outfile = NULL;
+	char *inputfile = NULL;
+	int tabstop = 8;
+
+
+	while( (c = getopt(argc, argv, "t:o:")) != -1 ){
+		switch( c ){
+			case 't':
+				tabstop = (int) strtol(optarg, NULL, 0);
+				break;
+
+			case 'o':
+				outfile = optarg;
+				break;
+
+			case 'h':
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+				break;
+
+			default:
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+				break;
+		}
+	}
+
+	if (optind > argc-2){
+		inputfile = argv[optind];
+	}
+
+	FILE *out_f;
+	FILE *in_f;
+
+	if (!outfile){
+		out_f = stdout;
+	} else {
+		out_f = fopen(outfile, "w");
+	}
+
+	if (!inputfile){
+		in_f = stdin;
+	} else {
+		in_f = fopen(inputfile, "r");
+	}
+
+	int next_c;
+
+	if (in_f) {
+		// feof, fileno both work
+		if (isatty(feof(in_f)) && !inputfile){ // fixed den hänger 
+			return EXIT_FAILURE;
+		}
+
+		while ((c = getc(in_f)) != EOF){ // programm hängt hier
+			if (c == '\\'){
+				if ((next_c = getc(in_f)) == 't'){
+					for (int i = 0; i < tabstop; ++i)
+					{
+						fputc(' ', out_f);
+					}
+				} else {
+					fputc(c, out_f);
+					ungetc(next_c, in_f);
+				}
+			} else {
+				fputc(c, out_f);
+			}
+		}
+		fclose(in_f);
+		fclose(out_f);
+	}
+	return EXIT_SUCCESS;
+}
+
+
