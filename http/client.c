ff1bd59ab8dd38d77d65679710981b6923723851
Jakob <e11809618@student.tuwien.ac.at>
2019-10-25 11:38:26 +0200

    update

--- http/client.c
+++ http/client.c
@@ -14,6 +14,12 @@
 #include <fcntl.h>
 #include <errno.h>
 
+
+typedef struct {
+	char *host;
+	char *resource;
+} request;
+
 void usage(char *myprog)
 {
 	fprintf(stderr, "Usage: %s [-p PORT] [-o FILE | -d DIR ] URL\n", myprog);
@@ -36,19 +42,31 @@ FILE *get_out(char *file, char *dir)
 	}
 }
 
+void GET(FILE *socket, char *host, char *file)
+{
+	fprintf(socket, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", file, host);
+	fflush(socket);
+}
+
+void parse_url(request *get_request, char *url)
+{
+	get_request->resource = "/";
+	get_request->host = url;
+}
+
 int main(int argc, char *argv[])
 {
 	int c;
-	char *outfile = "NULL";
-	char *directory = "NULL";
-	int port = 80;
+	char *outfile = NULL;
+	char *directory = NULL;
+	char *port = "80";
 	char *url = "localhost";
 
 	// reads in command line arguments
 	while( (c = getopt(argc, argv, "p:o:d:h")) != -1 ){
 		switch( c ){
 			case 'p':
-				port = (int) strtol(optarg, NULL, 0);
+				port = optarg;
 				break;
 
 			case 'o':
@@ -71,42 +89,95 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (optind > argc-2)
-	{
-		url = argv[optind];
-	}
+//	printf("argc: %d, optind: %d\n", argc, optind);
 
-	printf("%s:%d, writing to outfile %s or directory %s\n", url, port, outfile, directory);
+	// GET positional argument from cli	
+	url = argv[optind];
+	
+	//TODO remove
+	//printf("url: %s\nport: %s\n=====================\n", url, port);
 
-	//++++++++++++++++++++++++++++++++++++++++++++
 
 	struct addrinfo hints, *ai;
-
 	memset(&hints, 0, sizeof hints);
-
-	hints.ai_family = AF_INET;hints.ai_socktype = SOCK_STREAM;
-	int res = getaddrinfo(url, port, &hints, &ai);
-
-
-	if(res != 0) 
-	{
-		// error
-
-	}
-
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	getaddrinfo(url, port, &hints, &ai);
 	int sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+	connect(sockfd, ai->ai_addr, ai->ai_addrlen);
+	FILE *sockfp = fdopen(sockfd, "r+");
 
-	if(sockfd < 0) 
-	{
-		// error
+
+	//out = get_out(outfile, directory);
+	FILE *out = fopen("out.txt", "w");
+
+	if(out == NULL) {
+		fprintf(stderr, "fopen failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 
-	if(connect(sockfd, ai->ai_addr, ai->ai_addrlen) < 0) 
-	{
-		// error
+
+	fputs("Hello\n", out);	
+	fputs("World!\n", out);
+
+
+
+
+	request *rq = malloc(sizeof(request));
+
+	parse_url(rq, url);
+
+	GET(stdout, rq->host, rq->resource);
+	GET(sockfp, rq->host, rq->resource);
+
+
+	//char *buf = malloc(1024 * sizeof(char));
+	char buf[1024];
+
+    /*while (fgets(buf, sizeof(buf), sockfp) != NULL){
+		fputs(buf, stdout);
+        fputs(buf, out);
+        break;
+        //break;
+
+
+    }*/
+
+//	char buffer[2048];
+    int x;
+    //int y = 0;
+
+	while ((x = recv(sockfd, buf, sizeof(buf), 0)) > 0) {
+		printf("\n================================\nRead %d bytes\n================================\n", x);
+	    fputs(buf, stdout);
+	    //memcpy(buffer+y, buf, x);
+	    //y = y+x;
+	    memset(buf, 0, 1024);
+	
 	}
 
-	freeaddrinfo(ai);
+
+    //fputs(buffer, out);
+
+
+
+
+
+
+    
+    /*
+
+    int pos; cnt;
+    for (pos = 0; pos < sizeof(buf);){
+    	cnt = read(sockfpd, buf + pos, sizeof(buf) - pos);
+
+    	if 
+    }*/
+
+    fclose(sockfp);
+    fclose(out);
+//    free(buf);
+
 
 
 	return EXIT_SUCCESS;
--- http/server.c
+++ http/server.c
@@ -5,7 +5,14 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <errno.h>
 
 void usage(char *myprog)
 {
@@ -17,13 +24,13 @@ int main(int argc, char *argv[])
 {
 	int c;
 	char *index = "index.html";
-	int port = 8080;
+	char *port = "8080";
 
 	// reads in command line arguments
 	while( (c = getopt(argc, argv, "p:i:h")) != -1 ){
 		switch( c ){
 			case 'p':
-				port = (int) strtol(optarg, NULL, 0);
+				port = optarg;
 				break;
 
 			case 'i':
@@ -43,7 +50,56 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%s\n", index);
-	printf("%d\n", port);
+	printf("%s\n", port);
+
+
+	struct addrinfo hints, *ai;
+	memset(&hints, 0, sizeof hints);
+
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	int res = getaddrinfo( NULL, port, &hints, &ai);
+	if(res != 0) 
+	{
+		// error
+	}
+
+	int sockfd = socket(ai->ai_family, ai->ai_socktype,ai->ai_protocol);
+
+	if(sockfd < 0) 
+	{
+		// error
+	}
+
+	if(bind(sockfd, ai->ai_addr, ai->ai_addrlen) < 0) 
+	{
+		// error
+
+	}
+
+	if (listen(sockfd, 10) < 0)
+	{
+		// error
+	}
+
+	int new_socket; 
+
+	if ((new_socket = accept(sockfd, ai->ai_addr, &ai->ai_addrlen)) < 0){
+		// error
+	}
+
+	char buf[1024];
+
+    while (read(new_socket, buf, sizeof(buf)) > 0){
+		fputs(buf, stdout);
+
+
+    }
+
+	freeaddrinfo(ai);
 
 	return EXIT_SUCCESS;
 }
