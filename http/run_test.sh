900f0c10f2bec8f106d756adc453a4d2fc4749ab
Jakob_Guenther MAIER (OSUE participant) <y11809618@apps1.tilab.tuwien.ac.at>
2019-11-16 13:17:42 +0100

    update

--- http/run_test.sh
+++ http/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 port=8888
-host=127.0.0.1
+host=localhost
 dir=my_website/
 
 ./server -p $port $dir &
@@ -9,10 +9,28 @@ server_pid=$!
 ./client -p $port -o tmp.html http://${host}/  
 diff tmp.html $dir/index.html
 
+
+
 mkdir tmp_dir/
 ./client -p $port -d tmp_dir/ http://${host}/  
 diff $dir/index.html tmp_dir/index.html
 rm -rf tmp_dir/
 
-kill $server_pid
+wget http://pan.vmars.tuwien.ac.at/osue/index.html
+./client http://pan.vmars.tuwien.ac.at/osue/index.html > test.html
+diff index.html test.html
+wget http://pan.vmars.tuwien.ac.at/osue/countdown.js
+mv countdown.js wget_countdown.js
+./client -d . http://pan.vmars.tuwien.ac.at/osue/countdown.js
+diff countdown.js wget_countdown.js
+
+rm *.js 
+
+
+
+
+
+
+
+kill $server_pid || echo "error"
 
