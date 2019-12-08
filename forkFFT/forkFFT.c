56bb319b232865e1f9a88ec8d60cdfa14f28c41c
Jakob <e11809618@student.tuwien.ac.at>
2019-12-08 15:29:31 +0100

    update

--- forkFFT/forkFFT.c
+++ forkFFT/forkFFT.c
@@ -10,12 +10,12 @@
 #define OUTPUT 	0
 #define INPUT 	1  
 
-#define BUFSIZE 10
+#define BUFSIZE 128 
 
 #define EVEN 	0
 #define ODD 	1
 
-#define DEBUG 0 
+#define DEBUG 1 
 
 void complex_mult(complex x, complex y, complex *result)
 {
@@ -25,11 +25,13 @@ void complex_mult(complex x, complex y, complex *result)
 
 void debug(char *msg)
 {
+
 	if (DEBUG) fprintf(stderr, "(%d) %s\n", getpid(), msg);
 }
 
 void print_complex(complex *c)
 {
+
 	fprintf(stdout, "%f %f*i\n", c->a, c->b);
 }
 
@@ -70,7 +72,7 @@ void create_child(int *P, int *R, int *P1, int *R1)
 	close_all(R);
 	close_all(P);
 
-	execlp("./forkFFT", "./forkFFT", NULL);
+	execlp("./forkFFT", "./forkFFT", "0", NULL);
 	fprintf(stderr, "Failed to execute '\n");
 	exit(EXIT_FAILURE);
 }
@@ -78,11 +80,12 @@ void create_child(int *P, int *R, int *P1, int *R1)
 void read_pipe(int fd, complex *R, int n)
 {
 	int k = 0;
-	char buf[BUFSIZE];
+	char buf[100];
 	complex cval;
 	while (read(fd, buf, sizeof(buf)) != -1 && k < n/2){
-		debug("reading from even...");
+		fprintf(stderr, "reading from child: %s", buf);
 		parse_complex(buf, &cval);
+		//print_complex(&cval);
 		R[k] = cval; 
 		k++;
 	} 
@@ -90,14 +93,10 @@ void read_pipe(int fd, complex *R, int n)
 
 int main(int argc, char *argv[])
 {
-/*	int rec = 1;
-
-	if ( argc > 1){
-		rec = 0;	
-		fprintf(stderr, "(%d) recursion is turned off...\n", getpid());
+	int rec = 1;
+	if (argc > 1){
+		rec = 0;
 	}
-*/
-//	debug("was started")
 
 	int even_R[2];
 	int even_P[2];
@@ -106,18 +105,21 @@ int main(int argc, char *argv[])
 
 	int pid1, pid2;
 
-	// TODO check for return value
 	pipe(even_R);
 	pipe(even_P);
 	pipe(odd_R);
 	pipe(odd_P);
 
-	char buf[10];
-	char buffer[2][10];
+	char buf[BUFSIZE];
+	char buffer[2][BUFSIZE];
+
+	memset(buf, 0, sizeof(buf));
+	memset(buffer[0], 0, sizeof(buffer[0]));
+	memset(buffer[1], 0, sizeof(buffer[1]));
 
 	int n = 0;
-	while (fgets(buf, sizeof(buf), stdin) != NULL && memcmp(buf, "\0", 1) != 0){
-//		fprintf(stderr, "(%d) has read something... %s", getpid(), buf);	
+	while (fgets(buf, sizeof(buf), stdin) != NULL && strcmp(buf, "\n") != 0){
+		//fprintf(stderr, "(%d) has read something... %s", getpid(), buf);	
 
 		if (n % 2 == 0){
 			strncpy(buffer[EVEN], buf, 10);
@@ -125,15 +127,17 @@ int main(int argc, char *argv[])
 			strncpy(buffer[ODD], buf, 10);
 		}
 
-		if ( n == 1){ // TODO remove rec
-//			fprintf(stderr, "(%d) forking...\n", getpid());
+		if ( n == 1 && rec == 1){ // TODO remove rec
+			fprintf(stderr, "(%d) forking...\n", getpid());
 			pid1 = fork();
 			if (pid1 == 0){ // child 1
+				n = 0;
 				create_child(even_P, even_R, odd_P, odd_R);
 			} 
 
 			pid2 = fork();
 			if (pid2 == 0){ // child 2 
+				n = 0;
 				create_child(odd_P, odd_R, even_P, even_R);
 			} 
 
@@ -145,7 +149,7 @@ int main(int argc, char *argv[])
 
 		if ( n % 2 != 0){ // at least two values have been read
 //			fprintf(stderr, "(%d) %d values, writing...\n", getpid(), n+1);
-			write(odd_P[INPUT], buffer[ODD], 10);
+			write(odd_P[INPUT],  buffer[ODD], 10);
 			write(even_P[INPUT], buffer[EVEN], 10);
 		}
 		n++;
@@ -157,46 +161,59 @@ int main(int argc, char *argv[])
 
 	if (n == 1){
 		// could close all
+/*
 		close(odd_P[OUTPUT]);
 		close(odd_R[INPUT]);
 		close(even_R[INPUT]);
-		close(even_P[OUTPUT]);
+		close(even_P[OUTPUT]);*/
+
 
 //		fprintf(stderr, "(%d) read only one value, writing to stdout... %s \n", getpid(), buf);
 		float value = strtof(buffer[EVEN], NULL);
-
 		fprintf(stdout, "%f 0.0*i\n", value); 
-		//fprintf(stderr, "(%d) %f 0.0*i\n", getpid(), value); 
+		fprintf(stderr, "(%d) only one value %f 0.0*i\n", getpid(), value); 
+		close(STDOUT_FILENO);
 		exit(EXIT_SUCCESS);
-	}
-
-	int status1, status2;
-	waitpid(pid1, &status1, 0);
-	waitpid(pid2, &status2, 0);
-
-	if (WIFEXITED(status1)) {
-		if (WEXITSTATUS(status1) == 1 || WEXITSTATUS(status2) == 1){
-			exit_error("child exited with error");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-//	close(even_R[OUTPUT]);
-//	close(odd_R[OUTPUT]);
-
-	// wait until processes are finished
-
-	if (n % 2 != 0){
+	} else  if (n % 2 != 0){
 		exit_error("Input array is not even");
 	}
+
+	if (n == 0){
+		exit_error("did not read anything");
+	}
 	
-//	fprintf(stderr, "(%d) size of n: %d\n", getpid(), n);
+	int status1, status2;
+	do {
+		waitpid(pid1, &status1, 0);
+		if (WIFEXITED(status1)) {
+			if (WEXITSTATUS(status1) == 1){
+				exit_error("child exited with error");
+				exit(EXIT_FAILURE);
+			}
+		}
+	} while (!WIFEXITED(status1));
+	
+
+	do {
+		waitpid(pid2, &status2, 0);
+		if (WIFEXITED(status2)) {
+			if (WEXITSTATUS(status1) == 1){
+				exit_error("child exited with error");
+				exit(EXIT_FAILURE);
+			}
+		}
+	} while (!WIFEXITED(status2));
+
+
+	fprintf(stderr, "(%d) size of n: %d\n", getpid(), n);
 	
 	complex *R_e = malloc(sizeof(complex) * (n/2)); 
 	read_pipe(even_R[OUTPUT], R_e, n);
+	close(even_R[OUTPUT]);
 
 	complex *R_o = malloc(sizeof(complex) * (n/2));
 	read_pipe(odd_R[OUTPUT], R_o, n);
+	close(odd_R[OUTPUT]);
 
 	complex *R = malloc(sizeof(complex) * n);
 
@@ -216,12 +233,11 @@ int main(int argc, char *argv[])
 	free(R_o);
 
 	for (int i = 0; i < n; i++){
-//		fprintf(stderr, "writing output...\n");
+		fprintf(stderr, "writing output...\n");
 		print_complex(R+i);
 	}
 
 	free(R);
-
 
 	return EXIT_SUCCESS;
 }
