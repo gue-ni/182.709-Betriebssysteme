b735533e532e79b25aeb5de2f72b79ca462475ab
Jakob <e11809618@student.tuwien.ac.at>
2019-12-12 17:58:52 +0100

    update

--- forkFFT/forkFFT.c
+++ forkFFT/forkFFT.c
@@ -16,110 +16,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "forkFFT.h"
-
-#define OUTPUT 	0
-#define INPUT 	1  
-#define BUFSIZE (256)
-#define EVEN 	0
-#define ODD 	1
-#define DEBUG   0	
-
-void complex_mult(complex x, complex y, complex *result)
-{
-	result->a = x.a * y.a - x.b * y.b;
-	result->b = x.a * y.b + x.b * y.a;
-}
-
-void debug(char *msg)
-{
-	if (DEBUG) 
-		fprintf(stderr, "(%d) %s\n", getpid(), msg);
-}
-
-void print_complex(complex c)
-{
-	fprintf(stdout, "%.7f %.7f*i\n", c.a, c.b);
-}
-
-void print_complex_err(complex c)
-{
-	fprintf(stderr, "(%d) %.7f %.7f*i\n", getpid(), c.a, c.b);
-}
-
-void parse_complex(char *buf, complex *result)
-{
-	char *endptr;
-	endptr = buf;
-	result->a = strtof(endptr, &endptr);
-	result->b = strtof(endptr, NULL);
-}
-
-void parse_mult_complex(char *buf, complex *result, int n)
-{
-	char *endptr;
-	endptr = buf;
-	float a, b;
-
-	for (int i = 0; i < n; i++){
-		a = strtof(endptr, &endptr);
-		b = strtof(endptr, &endptr);
-		endptr += 3; // cut of *i \n
-		result[i].a = a;
-		result[i].b = b;
-
-		//printf("test: %f %f*i\n", result[i].a, result[i].b);
-	}
-}
-
-void close_all(int *fd)
-{
-	for (int i = 0; i < 2; i++){
-		close(fd[i]);
-	}
-}
-
-void exit_error(char *msg)
-{
-	fprintf(stderr, "%s\n", msg);
-	exit(EXIT_FAILURE);
-}
-
-void create_child(int *P, int *R, int *P1, int *R1)
-{
-	dup2(R[INPUT], STDOUT_FILENO);
-	dup2(P[OUTPUT], STDIN_FILENO);
-
-	close_all(R1);
-	close_all(P1);
-	close_all(R);
-	close_all(P);
-
-	execlp("./forkFFT", "./forkFFT", "0", NULL);
-	fprintf(stderr, "Failed to execute '\n");
-	exit(EXIT_FAILURE);
-}
-
-void read_child(int fd, complex *R, int n)
-{
-	char buf[1024]; // size of 1 complex number
-	for (int i = 0; read(fd, buf, sizeof(buf)) > 0; i++){
-		//fprintf(stderr, "(%d) reading from child: %s", getpid(), buf);
-
-		parse_mult_complex(buf, R+i, n);
-		fprintf(stderr, "(%d) reading from child: %f %f*i\n", getpid(), R[i].a, R[i].b);
-	}
-}
+#include "childIO.h"
 
 int main(int argc, char *argv[])
 {
-	int rec = 1, m = 0;
+	int rec = 1;
 	if (argc > 1){
 		rec = 1;
-		m = 0;
-	} else {
-		m = 1;
 	}
-
+	
 	int even_R[2];
 	int even_P[2];
 	int odd_R[2];
@@ -165,10 +70,13 @@ int main(int argc, char *argv[])
 
 			if (close(even_P[OUTPUT]) == -1)
 				exit_error("error closeing");
+
 			if(close(odd_P[OUTPUT]) == -1)
 				exit_error("error closing");
+
 			if(close(odd_R[INPUT]) == -1)
 				exit_error("error closing");
+
 			if(close(even_R[INPUT]) == -1)
 				exit_error("error closing");
 		}
@@ -181,7 +89,6 @@ int main(int argc, char *argv[])
 			if (write(even_P[INPUT], buffer[EVEN], strlen(buffer[EVEN])) == -1)
 				exit_error("error writing");
 
-			//if (DEBUG) fprintf(stderr, "(%d) wrote %d bytes\n", getpid(), wrote);
 
 		}
 		n++;
@@ -222,11 +129,13 @@ int main(int argc, char *argv[])
 	}
 
 	if (DEBUG) fprintf(stderr, "(%d) size of n: %d\n", getpid(), n);
-	
+
+	fprintf(stderr, "(%d) reading from even\n", getpid());	
 	complex *R_e = malloc(sizeof(complex) * (n/2)); 
 	read_child(even_R[OUTPUT], R_e, n/2);
 	close(even_R[OUTPUT]);
 
+	fprintf(stderr, "(%d) reading from odd\n", getpid());	
 	complex *R_o = malloc(sizeof(complex) * (n/2));
 	read_child(odd_R[OUTPUT], R_o, n/2);
 	close(odd_R[OUTPUT]);
@@ -241,7 +150,7 @@ int main(int argc, char *argv[])
 	if (1 && n > 1){
 		fprintf(stderr, "(%d) print odd:\n", getpid());
 		for (int i = 0; i < n/2; i++){
-			if (1) print_complex_err(R_e[i]);
+			if (1) print_complex_err(R_o[i]);
 		} 
 	}
 
@@ -268,7 +177,7 @@ int main(int argc, char *argv[])
 
 	if (0) fprintf(stderr, "(%d) output:\n", getpid());
 	for (int i = 0; i < n; i++){
-		if (1) fprintf(stderr, "(%d) writing output: %f %f*i\n", getpid(), R[i].a, R[i].b);
+		if (1) fprintf(stderr, "(%d) writing calculated output: %f %f*i\n", getpid(), R[i].a, R[i].b);
 		print_complex(R[i]);
 	}
 
