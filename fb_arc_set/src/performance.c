6b988a65329b20c382e726587f923fe13aabedf8
Jakob <e11809618@student.tuwien.ac.at>
2019-12-20 10:23:11 +0100

    update to performance testing

--- fb_arc_set/src/performance.c
+++ fb_arc_set/src/performance.c
@@ -8,27 +8,40 @@ int main( int argc, char *argv[])
 
 	struct timespec start, stop;
 	double accum;
+	double sum;
 
-	clock_gettime(CLOCK_REALTIME, &start);
 
 	long int fac;
 
-	for (int e = 0; e < 1E7; e++){
-		fac = 1;
-		for (int i = 1; i < 10; i++){
-			fac *= i;
+	for (int j = 0; j < 15; j++){
+		clock_gettime(CLOCK_REALTIME, &start);
+
+		for (int e = 0; e < 1E7; e++){
+			fac = 1;
+			for (int i = 1; i < 10; i++){
+				fac *= i;
+			}
+
 		}
+
+		clock_gettime(CLOCK_REALTIME, &stop);
+
+		accum = ( stop.tv_sec - start.tv_sec )
+	          + ( stop.tv_nsec - start.tv_nsec )
+	            / 1E9;
+
+	    printf("%lf\n", accum);
+
+	    sum += accum;
+
+
+
 
 	}
 
-	clock_gettime(CLOCK_REALTIME, &stop);
 
-	accum = ( stop.tv_sec - start.tv_sec )
-          + ( stop.tv_nsec - start.tv_nsec )
-            / 1E9;
-
-    printf( "%lf\n", accum );
-
+	
+    printf( "average: %lf\n", sum / 15);
 
     return(EXIT_SUCCESS);
 }
