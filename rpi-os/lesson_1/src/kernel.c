2b6677d4c5e875a736be78b91265b6a6f36822ee
Jakob <e11809618@student.tuwien.ac.at>
2019-12-25 11:34:24 +0100

    update to kernel

--- rpi-os/lesson_1/src/kernel.c
+++ rpi-os/lesson_1/src/kernel.c
@@ -3,7 +3,7 @@
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
+	uart_send_string("Hello, I'm the raspberry pi kernel!\r\n");
 
 	while (1) {
 		uart_send(uart_recv());
